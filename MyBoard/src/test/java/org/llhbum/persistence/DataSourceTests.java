a88d98d417d6a77bc52efd6e1eabff3e1f6bf53d
llhbum <llhbum@gmail.com>
2021-01-13 18:29:35 +0900

    210113_Myboard_init Commit

---
+++ MyBoard/src/test/java/org/llhbum/persistence/DataSourceTests.java
@@ -0,0 +1,41 @@
+package org.llhbum.persistence;
+
+import static org.junit.Assert.fail;
+import java.sql.Connection;
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class DataSourceTests {
+	@Setter(onMethod=@__({@Autowired}))
+		private DataSource dataSource;
+	@Setter(onMethod=@__({@Autowired}))
+		private SqlSessionFactory SqlSessionfactory;
+	
+		@Test
+		public void testmybatis() {
+			
+			try(SqlSession session = SqlSessionfactory.openSession();
+					Connection con = session.getConnection()){
+				log.info(session);
+				log.info("성공"+con);
+				
+				
+			}catch (Exception e) {
+				// TODO: handle exception
+				fail(e.getMessage());
+			}
+		}
+}
