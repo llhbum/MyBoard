11fd9c4ad2a99894f9becba3ee6127190c5bf636
llhbum <llhbum@gmail.com>
2021-01-25 01:06:20 +0900

    210126 AOP설정

--- MyBoard/src/main/java/org/llhbum/controller/ReplyController.java
+++ MyBoard/src/main/java/org/llhbum/controller/ReplyController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.llhbum.domain.Criteria;
+import org.llhbum.domain.ReplyPageDTO;
 import org.llhbum.domain.ReplyVO;
 import org.llhbum.service.ReplyService;
 
@@ -45,19 +46,19 @@ public class ReplyController {
 	}
 	
 	//getList
-	@GetMapping(value = "/pages/{bno}/{page}",
+	@GetMapping(value ="/pages/{bno}/{page}" , 
 			produces = {
 					MediaType.APPLICATION_XML_VALUE,
-					MediaType.APPLICATION_JSON_UTF8_VALUE } )
-	public ResponseEntity<List<ReplyVO>> getList(
+					MediaType.APPLICATION_JSON_UTF8_VALUE
+			})
+	public ResponseEntity<ReplyPageDTO>getList(
 			@PathVariable("page")int page,
-			@PathVariable("bno")Long bno){
+			@PathVariable("bno")Long bno) {
 		
-		Criteria cri = new Criteria(page,10);
-		log.info(cri);
-		
-		return new ResponseEntity<List<ReplyVO>>(service.getList(cri, bno), HttpStatus.OK);
-		
+				log.info("getList................");
+				Criteria cri = new Criteria(page,10);
+				log.info(cri);
+			return new ResponseEntity<>(service.getListPage(cri, bno), HttpStatus.OK);
 	}
 	
 	//get
--- MyBoard/src/main/webapp/resources/js/reply.js
+++ MyBoard/src/main/webapp/resources/js/reply.js
@@ -1,101 +1,103 @@
-/** 
- * 
- */
- 
- console.log("Reply Module....");
- var replyService = (function(){
- 
- 	function add(reply, callback, error){
- 		console.log("add reply..........");
- 		
- 		$.ajax({
- 			type : 'post',
- 			url : '/replies/new',
- 			data : JSON.stringify(reply),
- 			contentType : "application/json; charset=utf-8",
- 			success : function(result, status, xhr){
- 				if(callback){
- 				callback(result);
- 				}
- 			},
- 			error : function(xhr, status, er){
- 				if(error){
- 					error(er);
- 				}
- 			}
- 		
- 		});
 
- 	}
- 	
- 	function getList(param, callback, error){
- 		var bno = param.bno;
- 		var page = param.page || 1;
- 		
- 		$.getJSON("/replies/pages/" + bno + "/" + page + ".json",
- 			function(data){
- 				if(callback){
- 					callback(data);
- 				}
- 			}).fail(function(xhr,status,err){
- 				if(error){
- 					error();
- 				}
- 			});
- 		}
- 		
- 	function remove(rno, callback, error){
- 		$.ajax({
- 			type : 'delete',
- 			url : '/replies/' + rno,
- 			success : function(deleteResult, status, xhr){
- 				if(callback){
- 				callback(deleteResult);
- 				}
- 			},
- 			error : function(xhr, status, er){
- 				if(error){
- 					error(er);
- 				}
- 			}
- 		
- 		});
- 	}
- 	
- 	function update(reply, callback, error){
- 		console.log("RNO" + reply.rno);
- 		
- 		$.ajax({
- 			type : 'put',
- 			url : '/replies/' + reply.rno,
- 			data : JSON.stringify(reply),
- 			contentType : "application/json; charset=utf-8",
- 			success : function(result, status, xhr){
- 				if(callback){
- 				callback(result);
- 				}
- 			},
- 			error : function(xhr, status, er){
- 				if(error){
- 					error(er);
- 				}
- 			}
- 		});
- 	}
- 	
- 	function get(rno, callback,error){
- 		$.get("/replies/" + rno + ".json" , function(result) {
- 			if(callback){
- 				callback(result);
- 			}
- 		
- 		}).fail(function(xhr, status, err){
- 			if(error){
- 				error();
- 			}
- 		});
- 	}
- 	
+ 
+var replyService = (function () {
+ 	console.log("replyService Test.............");
+    //댓글 등록 처리
+    function add(reply, callback, error) {
+        console.log("add reply...............");
+ 
+        $.ajax({
+            type: 'post',
+            url: '/replies/new',
+            data: JSON.stringify(reply),
+            contentType: "application/json; charset=utf-8",
+            success: function (result, status, xhr) {
+                if (callback) {
+                    callback(result);
+                }
+            },
+            error: function (xhr, status, er) {
+                if (error) {
+                    error(er);
+                }
+            }
+        })
+    }
+ 
+    //댓글 목록 처리
+    function getList(param, callback, error) {
+ 		console.log("getList..............")
+        var bno = param.bno;
+        var page = param.page || 1;
+ 
+        $.getJSON("/replies/pages/" + bno + "/" + page + ".json",
+            function (data) {
+                if (callback) {
+                    //callback(data);
+                    callback(data.replyCnt, data.list); //댓글 숫자와 목록을 가져오는 경우
+                }
+            }).fail(function (xhr, status, err) {
+            if (error) {
+                error();
+            }
+        });
+    }
+ 
+    //댓글 삭제
+    function remove(rno, callback, error) {
+        $.ajax({
+            type: 'delete',
+            url: '/replies/' + rno,
+            success: function (deleteResult, status, xhr) {
+                if (callback) {
+                    //callback(data);
+                    callback(deleteResult); //댓글 숫자와 목록을 가져오는 경우
+                }
+            },
+            error: function (xhr, status, er) {
+                if (error) {
+                    error(er);
+                }
+            }
+        });
+    }
+ 
+    //댓글 수정
+    function update(reply, callback, error) {
+ 
+        console.log("댓글 번호 rno : " + reply.rno);
+ 
+        $.ajax({
+            type: 'put',
+            url : '/replies/' + reply.rno,
+            data : JSON.stringify(reply),
+            contentType : "application/json; charset=utf-8",
+            success : function (result, status, xhr) {
+                if (callback) {
+                    callback(result);
+                }
+            },
+            error : function (xhr, status, er) {
+                if (error) {
+                    error(er);
+                }
+            }
+        });
+    }
+ 
+    //댓글 조회 처리
+    function get(rno, callback, error) {
+        $.get("/replies/" + rno + ".json", function (result) {
+            if (callback) {
+                callback(result);
+            }
+        }).fail(function (xhr, status, err) {
+            if (error) {
+                error();
+            }
+        });
+    }
+ 
     function displayTime(timeValue) {
  
         var today = new Date();
@@ -119,12 +121,13 @@
     }
  
  
- 	return {
-	 	add:add,
-	 	getList:getList,
-	 	remove : remove,
-	 	update : update,
-	 	get : get,
-	 	displayTime : displayTime
- 	}; 
- })();
+ 
+    return {
+        add: add,
+        get : get,
+        getList: getList,
+        remove: remove,
+        update: update,
+        displayTime : displayTime
+    };
+})();
