5eb72384efa6bfe61038236108714425dd7377db
llhbum <llhbum@gmail.com>
2021-01-27 20:57:02 +0900

    test

--- MyBoard/src/main/java/org/llhbum/controller/UploadController.java
+++ MyBoard/src/main/java/org/llhbum/controller/UploadController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
+import lombok.extern.java.Log;
 import lombok.extern.log4j.Log4j;
 
 @Controller
@@ -82,6 +83,7 @@ public class UploadController {
 	@GetMapping("/test")
 	public void test() {
 		log.info("test");
+		log.info("test22");
 	}
 	
 
